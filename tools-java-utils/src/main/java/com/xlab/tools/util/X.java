b2f79b95f930433687bc42b1a7c528187cb55a5f
towan <tongwenzide@163.com>
2017-03-17 10:52:37 +0800

    add for empty project

---
+++ tools-java-utils/src/main/java/com/xlab/tools/util/X.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 创新工作X实验室  @ 2017 版权所有
+ */
+package com.xlab.tools.util;
+
+/**
+ * <p>测试占位</p>
+ * @author towan  
+ */
+public class X {
+    
+    /**
+     * 测试
+     */
+    public void sayHello(){
+        System.out.println("hello world");
+    }
+}
